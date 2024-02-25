3bea9e35219f58b1a4940cdeda0e76c7aa79bbc2
Gilles FELIX <gilles@famille-felix.fr>
2024-02-25 22:46:01 +0100

    common

--- common.js
+++ common.js
@@ -1,2 +1,9 @@
+// ==UserScript==
+// @name         Common
+// @description  Common script
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+// ==/UserScript==
+
 const my_css = GM_getResourceText("Gilles_css");
 GM_addStyle(my_css);

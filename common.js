e7bfe6d8a915517711eb5072b69ebbab53f60601
Gilles FELIX <gilles@famille-felix.fr>
2024-02-25 22:44:58 +0100

    common

--- common.js
+++ common.js
@@ -1,13 +1,2 @@
-function GM_addStyle(css) {
-    const style = document.getElementById("GM_addStyleBy8626") || (function() {
-        const style = document.createElement('style');
-        style.type = 'text/css';
-        style.id = "GM_addStyleBy8626";
-        document.head.appendChild(style);
-        return style;
-    })();
-    const sheet = style.sheet;
-    sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
-}
-
-console.log('ici');
+const my_css = GM_getResourceText("Gilles_css");
+GM_addStyle(my_css);

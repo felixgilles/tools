b7473323061ab41014ebec822638bf4b20197fca
Gilles FELIX <gilles@famille-felix.fr>
2024-02-25 22:28:29 +0100

    common

--- common.js
+++ common.js
@@ -1,1 +1,13 @@
+function GM_addStyle(css) {
+    const style = document.getElementById("GM_addStyleBy8626") || (function() {
+        const style = document.createElement('style');
+        style.type = 'text/css';
+        style.id = "GM_addStyleBy8626";
+        document.head.appendChild(style);
+        return style;
+    })();
+    const sheet = style.sheet;
+    sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
+}
+
 console.log('ici');
